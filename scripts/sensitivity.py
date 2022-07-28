344e56e64754a8d86445154c3938881e62f2c7b5
Simon Zwieback <szwieback@alaska.edu>
2022-07-28 12:24:13 -0800

    sensitivity plot labels

--- scripts/sensitivity.py
+++ scripts/sensitivity.py
@@ -56,24 +56,50 @@ def toolik_sensitivity():
         profiles['sensitivity'].append(strat_sim_sens.stratigraphy['e'])
 
     import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+    import datetime
     from scripts.plotting import prepare_figure, colslist
-    fig, axs = prepare_figure(ncols=len(e_sim), nrows=4, sharex='row', sharey='row')
-    days = np.arange(results['baseline']['s'].shape[1])
+    ylims = [(0.05, 0.00),(0.8, 0.0),(0.015, -0.015)]
+    yticks = [(0.00, 0.02, 0.04), (0.0, 0.3, 0.6), (-0.01, 0.00, 0.01)]
+    yticklabels = [(0, 2, 4), (0, 30, 60), (-1, 0, 1)]
+    ylabels = ['$s(t)$ [cm]', '$y_f(t)$ [cm]', '$\\delta y_f(t)$ [cm]']
+    fig, axs = prepare_figure(
+        ncols=len(e_sim), nrows=3, figsize=(0.7, 0.65), sharex='col', bottom=0.1, 
+        left=0.17, right=0.98)
+    _days = np.arange(results['baseline']['s'].shape[1])
+    days = [d0_ + datetime.timedelta(days=int(d)) for d in _days]
+
+    for jax, ax in enumerate(axs):
+        ax.axhline(0.0, c='#dddddd', lw=0.4)
+        ax.set_ylim(ylims[jax])
+        ax.set_yticks(yticks[jax])
+        ax.set_yticklabels(yticklabels[jax])
+        ax.text(
+            -0.13, 0.50, ylabels[jax], transform=ax.transAxes, ha='right', va='center', 
+            rotation=90)
+
     for nsim in range(len(e_sim)):
         for jdist, ind in enumerate(ind_dist):
-            c, lw = colslist[jdist + 1], 0.6
+            c, lw = colslist[jdist + 1], 0.8
             axs[0].plot(days, results['sensitivity'][jdist]['s'][nsim, :], c=c, lw=lw)
             dyf = (results['sensitivity'][jdist]['yf'][nsim, :]
                     -results['baseline']['yf'][nsim, :])
+            axs[1].axhspan(
+                ygrid[ind - hind], ygrid[ind + hind], edgecolor='none', facecolor=c, 
+                alpha=0.12)
             axs[2].plot(days, dyf, c=c, lw=lw)
-            axs[3].plot(profiles['sensitivity'][jdist][nsim, :], ygrid, c=c, lw=lw)
 #             ds = (results['sensitivity'][jdist]['s'][nsim, :]
 #                   -results['baseline']['s'][nsim, :])
 #             axs[4, nsim].plot(days[1:], np.diff(ds), c=c, lw=lw)
-        c, lw = colslist[0], 1.2
+        c, lw = colslist[0], 1.1
         axs[0].plot(days, results['baseline']['s'][nsim, :], c=c, lw=lw)
         axs[1].plot(days, results['baseline']['yf'][nsim, :], c=c, lw=lw)
-        axs[3].plot(profiles['baseline'][nsim, :], ygrid, c=c, lw=lw)
+    axs[0].xaxis.set_major_locator(mdates.MonthLocator(interval=1))
+    axs[0].xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+    axs[0].set_xlim((days[0], days[-1]))
+    axs[1].text(0.60, 0.02, 'baseline', transform=axs[1].transAxes)
+    axs[2].text(0.99, 0.11, 'shallow', transform=axs[2].transAxes, ha='right')
+    axs[2].text(0.99, 0.89, 'deep', transform=axs[2].transAxes, ha='right')
 
     plt.show()
 
