17a6e2cbb7a89cda0d980776ee7f51844037f877
Simon Zwieback <szwieback@alaska.edu>
2023-05-15 12:34:02 -0800

    modular sensitivity script

--- scripts/sensitivity.py
+++ scripts/sensitivity.py
@@ -8,13 +8,12 @@ import numpy as np
 import pandas as pd
 import copy
 
-from scripts.synthetic_simulation import parse_dates
 from scripts.pathnames import paths
 from analysis import StefanPredictor, InversionSimulator, PredictionEnsemble, load_object
 from simulation import (StefanStratigraphyPrescribedConstantE)
 
 def toolik_sensitivity(fnout):
-    from forcing import read_toolik_forcing
+    from forcing import read_toolik_forcing, parse_dates
 
     fnforcing = os.path.join(paths['forcing'], 'toolik2019', '1-hour_data.csv')
     pathout = os.path.join(paths['simulation'], 'sensitivity')
@@ -24,17 +23,40 @@ def toolik_sensitivity(fnout):
     d0_, d1_ = parse_dates((d0, d1), strp='%Y-%m-%d')
     dailytemp = (df.resample('D').mean())['air_temp_5m'][pd.date_range(start=d0, end=d1)]
     dailytemp[dailytemp < 0] = 0
+    geom = {'ia': 0.0}
+    e_sim = [0.05],  # [0.01, 0.1]
+    ind_dist = [75, 200]
 
+    results, profiles, meta = compute_sensitivity(e_sim, geom, dailytemp, ind_dist)
+
+    plot_sensitivity(e_sim, results, d0_, meta, fnout=fnout)
+
+def sagwon_sensitivity(fnout):
+    from forcing import read_daily_noaa_forcing, parse_dates
+
+    fnforcing = os.path.join(paths['forcing'], 'sagwon', 'sagwon.csv')
+    pathout = os.path.join(paths['simulation'], 'sensitivity')
+
+    df = read_daily_noaa_forcing(fnforcing, convert_temperature=False)
+    d0, d1 = '2019-05-18', '2019-09-17'
+    d0_, d1_ = parse_dates((d0, d1), strp='%Y-%m-%d')
+    dailytemp = (df.resample('D').mean())[pd.date_range(start=d0, end=d1)]
+    dailytemp[dailytemp < 0] = 0
+    geom = {'ia': 0.0}
+    e_sim = [0.05],  # [0.01, 0.1]
+    ind_dist = [75, 200]
+
+    results, profiles, meta = compute_sensitivity(e_sim, geom, dailytemp, ind_dist)
+
+    plot_sensitivity(e_sim, results, d0_, meta, fnout=fnout)
+
+def compute_sensitivity(e_sim, geom, dailytemp, ind_dist, hind=20):
+    predictor = StefanPredictor()
     results = {}
     profiles = {}
-
-    geom = {'ia': 0.0}
-    e_sim = [0.05],# [0.01, 0.1]
-    predictor = StefanPredictor()
-
     strat_sim = StefanStratigraphyPrescribedConstantE(N=len(e_sim))
     strat_sim.draw_stratigraphy()
-    strat_sim.stratigraphy['e'][:, :] = np.array(e_sim)[:, np.newaxis]
+    strat_sim.stratigraphy['e'][:,:] = np.array(e_sim)[:, np.newaxis]
     predens_sim = PredictionEnsemble(strat_sim, predictor, geom=geom)
     predens_sim.predict(dailytemp)
     results['baseline'] = predens_sim.results
@@ -42,10 +64,7 @@ def toolik_sensitivity(fnout):
     ygrid = strat_sim._ygrid
 
     results['sensitivity'], profiles['sensitivity'] = [], []
-    ind_dist = [75, 250]
-    hind = 20
     y_dist = [ygrid[ind] for ind in ind_dist]
-    print(y_dist)
 
     for ind in ind_dist:
         strat_sim_sens = copy.deepcopy(strat_sim)
@@ -54,61 +73,65 @@ def toolik_sensitivity(fnout):
         predens_sim_sens.predict(dailytemp)
         results['sensitivity'].append(predens_sim_sens.results)
         profiles['sensitivity'].append(strat_sim_sens.stratigraphy['e'])
+    meta = {'y_dist': y_dist, 'ind_dist': ind_dist, 'ygrid': ygrid, 'hind': hind}
+    return results, profiles, meta
 
+def plot_sensitivity(e_sim, results, d0_, meta, fnout=None):
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
     import datetime
     from scripts.plotting import prepare_figure, colslist
     from string import ascii_lowercase
-    ylims = [(0.05, 0.00),(0.8, 0.0),(0.015, -0.015)]
-    yticks = [(0.00, 0.02, 0.04), (0.0, 0.3, 0.6), (-0.01, 0.00, 0.01)]
-    yticklabels = [(0, 2, 4), (0, 30, 60), (-1, 0, 1)]
+    ylims = [(0.04, 0.00), (0.6, 0.0), (0.05, -0.05)]
+
+    yticks = [(0.00, 0.02, 0.04), (0.0, 0.3, 0.6), (-0.05, 0.00, 0.05)]
+    yticklabels = [(0, 2, 4), (0, 30, 60), (-5, 0, 5)]
     ylabels = ['$s(t)$ [cm]', '$y_f(t)$ [cm]', '$\\delta y_f(t)$ [cm]']
     fig, axs = prepare_figure(
-        ncols=len(e_sim), nrows=3, figsize=(0.7, 0.65), sharex='col', bottom=0.1, 
+        ncols=len(e_sim), nrows=3, figsize=(0.7, 0.65), sharex='col', bottom=0.1,
         left=0.17, right=0.98)
     _days = np.arange(results['baseline']['s'].shape[1])
     days = [d0_ + datetime.timedelta(days=int(d)) for d in _days]
-
+    print(meta['ygrid'][0:2])
     for jax, ax in enumerate(axs):
         ax.axhline(0.0, c='#dddddd', lw=0.4)
         ax.set_ylim(ylims[jax])
         ax.set_yticks(yticks[jax])
         ax.set_yticklabels(yticklabels[jax])
         ax.text(
-            -0.13, 0.50, ylabels[jax], transform=ax.transAxes, ha='right', va='center', 
+            -0.13, 0.50, ylabels[jax], transform=ax.transAxes, ha='right', va='center',
             rotation=90)
         ax.text(
-            0.02, 0.07, f'{ascii_lowercase[jax]})', ha='left', va='baseline', 
+            0.02, 0.07, f'{ascii_lowercase[jax]})', ha='left', va='baseline',
             transform=ax.transAxes)
-    
+    ygrid, hind = meta['ygrid'], meta['hind']
     for nsim in range(len(e_sim)):
-        for jdist, ind in enumerate(ind_dist):
+        for jdist, ind in enumerate(meta['ind_dist']):
             c, lw = colslist[jdist + 1], 0.8
-            axs[0].plot(days, results['sensitivity'][jdist]['s'][nsim, :], c=c, lw=lw)
-            dyf = (results['sensitivity'][jdist]['yf'][nsim, :]
-                    -results['baseline']['yf'][nsim, :])
+            axs[0].plot(days, results['sensitivity'][jdist]['s'][nsim,:], c=c, lw=lw)
+            dyf = (results['sensitivity'][jdist]['yf'][nsim,:]
+                    -results['baseline']['yf'][nsim,:])
             axs[1].axhspan(
-                ygrid[ind - hind], ygrid[ind + hind], edgecolor='none', facecolor=c, 
+                ygrid[ind - hind], ygrid[ind + hind], edgecolor='none', facecolor=c,
                 alpha=0.12)
             axs[2].plot(days, dyf, c=c, lw=lw)
 #             ds = (results['sensitivity'][jdist]['s'][nsim, :]
 #                   -results['baseline']['s'][nsim, :])
 #             axs[4, nsim].plot(days[1:], np.diff(ds), c=c, lw=lw)
         c, lw = colslist[0], 1.1
-        axs[0].plot(days, results['baseline']['s'][nsim, :], c=c, lw=lw)
-        axs[1].plot(days, results['baseline']['yf'][nsim, :], c=c, lw=lw)
+        axs[0].plot(days, results['baseline']['s'][nsim,:], c=c, lw=lw)
+        axs[1].plot(days, results['baseline']['yf'][nsim,:], c=c, lw=lw)
     axs[0].xaxis.set_major_locator(mdates.MonthLocator(interval=1))
     axs[0].xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
     axs[0].set_xlim((days[0], days[-1]))
-    axs[1].text(0.60, 0.02, 'baseline', transform=axs[1].transAxes)
-    axs[2].text(0.99, 0.11, 'shallow', transform=axs[2].transAxes, ha='right')
-    axs[2].text(0.99, 0.89, 'deep', transform=axs[2].transAxes, ha='right')
-    plt.savefig(fnout)
-    
+    axs[1].text(0.50, 0.02, 'baseline', transform=axs[1].transAxes)
+    axs[2].text(0.44, 0.99, 'shallow', transform=axs[2].transAxes, ha='right')
+    axs[2].text(0.99, 0.99, 'deep', transform=axs[2].transAxes, ha='right')
+    if fnout is not None:
+        fig.savefig(fnout)
+    else:
+        plt.show()
 
 if __name__ == '__main__':
-    from scripts.pathnames import paths
-    import os
-    toolik_sensitivity(os.path.join(paths['figures'], 'sensitivity.pdf'))
+    sagwon_sensitivity(os.path.join(paths['figures'], 'sensitivity.pdf'))
 
