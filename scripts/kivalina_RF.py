aa04590a55254ddc5def6c8893a294c1f37e7ce4
Simon Zwieback <szwieback@alaska.edu>
2023-08-30 18:02:20 -0800

    RF plots

--- scripts/kivalina_RF.py
+++ scripts/kivalina_RF.py
@@ -8,6 +8,10 @@ import joblib
 
 path0 = '/home/simon/Work/gie/ancillary/GEE/'
 y_variable = 'ebar'
+
+df = pd.read_csv(os.path.join(path0, 'sample.csv'), index_col=0)
+df.dropna(subset=[y_variable], inplace=True)
+
 def xy_split(df, y_variable=y_variable, train_test=True):
     X = pd.get_dummies(df.drop(y_variable, axis=1))
     y = df[y_variable]
@@ -15,7 +19,7 @@ def xy_split(df, y_variable=y_variable, train_test=True):
         return X, y
     else:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12345)
-        return X_train, X_test, y_train, y_test 
+        return X_train, X_test, y_train, y_test
 
 def variable_importance(df, fnrf, fnimp):
     rfr = joblib.load(fnrf)
@@ -29,7 +33,7 @@ def variable_importance(df, fnrf, fnimp):
     fip = result.importances_mean
     impres = {'names': names, 'permutation': fip, 'standard': fi}
     joblib.dump(impres, fnimp)
-    
+
 def run_RF(df, search=False, fnout=None):
     X_train, X_test, y_train, y_test = xy_split(df, train_test=True)
     if search:
@@ -47,7 +51,7 @@ def run_RF(df, search=False, fnout=None):
                     )
                     print("Parameters: {0}".format(results["params"][candidate]))
                     print("")
-    
+
         from sklearn.model_selection import GridSearchCV
         n_estimators = [200, 300, 400]
         max_features = [3, 4, 5]
@@ -59,17 +63,17 @@ def run_RF(df, search=False, fnout=None):
         grid = GridSearchCV(rfr, param_grid)
         grid.fit(X_train, y_train)
         report(grid.cv_results_)
-    
+
     # Fit Random Forest Regressor with tuned hyperparameters
     rfr = RandomForestRegressor(n_estimators=300, max_features=5, bootstrap=True, oob_score=True)
     rfr.fit(X_train, y_train)
     predictions = rfr.predict(X_test)
-    
+
     # Calculate RMSE on test sample
     from sklearn.metrics import mean_squared_error
     RMSE = mean_squared_error(y_test, predictions, squared=False)  # If True returns MSE value, if False returns RMSE value.
     print(f'RMSE = {np.round(RMSE,3)}')
-    
+
     # Retrain on entire sample
     X, y = xy_split(df, train_test=False)
     rfr.fit(X, y)
@@ -77,86 +81,71 @@ def run_RF(df, search=False, fnout=None):
     if fnout is not None:
         joblib.dump(rfr, fnout)
 
-def plot_fit(df, rfr, impres):
-    import matplotlib.pyplot as plt
-    from scripts.plotting import prepare_figure
-    fig, axs = prepare_figure(ncols=2, figsize=(1.0, 0.4), sharex=False, sharey=False, wspace=1)
-    X, y = xy_split(df, train_test=False)
-    axs[0].plot(y, rfr.predict(X), linestyle='none', mec='none', mfc='k', ms=1, marker='o', alpha=0.1)
-    fitype = 'permutation'
-    ind = np.argsort(impres[fitype])
-    print(impres[fitype])
-    axs[1].plot(impres[fitype][ind], np.arange(len(ind)), linestyle='none', marker='o')
-    axs[1].set_yticks(np.arange(len(ind)))
-    axs[1].set_yticklabels(np.array(df.columns)[1:][ind])
-    axs[1].set_xlim((0.0, 0.7))
-    plt.show()
-    
+
 def X_pred(df, variables, fixed_values=None, N=64, percentiles=(5, 95)):
     if fixed_values is None:
         fixed_values = {'northerliness': 0.0, 'easterliness': 1.0}
 
     covariates = [key for key in df.columns if key != y_variable]
-    X = np.zeros((N*N, len(covariates)))
+    X = np.zeros((N * N, len(covariates)))
     consts = [key for key in covariates if (key not in variables)]
 
     gridvar = [np.linspace(*np.nanpercentile(df[var], percentiles), num=N) for var in variables]
-    gridvar_m = np.array(np.meshgrid(*gridvar)).reshape(2, N*N)
+    gridvar_m = np.array(np.meshgrid(*gridvar)).reshape(2, N * N)
     for jvar, var in enumerate(variables):
-        X[:, covariates.index(var)]= gridvar_m[jvar, :]
+        X[:, covariates.index(var)] = gridvar_m[jvar,:]
     for _const in consts:
         val = np.nanmedian(df[_const])
         if _const in fixed_values:
             val = fixed_values[_const]
-        X[:, covariates.index(_const)] = val  
+        X[:, covariates.index(_const)] = val
     return gridvar, pd.DataFrame(data=X, columns=covariates)
-    
-def predict(df, rfr, variables, fixed_values=None, N=64, percentiles=(5, 95)):
+
+def predict_grid(df, rfr, variables, fixed_values=None, N=64, percentiles=(1, 99)):
     gridvar, X_p = X_pred(df, variables, fixed_values=fixed_values, N=N, percentiles=percentiles)
     y_p = np.reshape(rfr.predict(X_p), (len(gridvar[0]), len(gridvar[1])))
     return gridvar, y_p
-    
-def plot_pred(df, rfr):
-    vlim = (0.0, 0.5)
-    variables = ('slope', 'NDWI')
-    fixed_values = {'northerliness': 0.0, 'easterliness': 1.0, 'DEM_bp': 0}
-    fixed_values_plot = [{'ndvi': 0.2}, {'ndvi': 0.5}, {'ndvi': 0.8}]
-    
-    import matplotlib.pyplot as plt
-    from scripts.plotting import prepare_figure, cmap_e
-    fig, axs = prepare_figure(
-        ncols=3, figsize=(1.6, 0.5), sharex=False, sharey=False, wspace=1, bottom=0.22)
-    for jp, _fv in enumerate(fixed_values_plot):
-        fv = {**fixed_values, **_fv}
-        gridvar, y_p = predict(df, rfr, variables, fixed_values=fv)
-        axs[jp].imshow(
-            y_p, extent=(gridvar[0][0], gridvar[0][-1], gridvar[1][0], gridvar[1][-1]), aspect='auto',
-            vmin=vlim[0], vmax=vlim[1], cmap=cmap_e)
-        axs[jp].set_xlabel(variables[0])
-        axs[jp].set_ylabel(variables[1])
-    plt.show()
-        
-    
-    
-    
+
+
+
+def predict_image(fnim, rfr, fnout):
+    # make prediction over entire image and plot
+    from analysis import Geospatial
+    import rasterio
+    import warnings
+    from rasterio.windows import Window
+    geospatial = Geospatial.from_file(fnim)
+    print(geospatial)
+    with rasterio.open(fnim) as src:
+        shape = (src.height, src.width)
+        with rasterio.open(
+            fnout, 'w', driver='GTiff', width=shape[1], height=shape[0], count=1,
+            dtype=np.float32, crs=src.crs, transform=src.transform) as dst:
+            for row in range(src.height):
+                with warnings.catch_warnings():
+                    warnings.simplefilter('ignore')
+                    window = Window(0, row, shape[1], 1)
+                    X = np.moveaxis(src.read(None, window=window), 0, -1)[0, ...]
+                    invalid = np.any(np.isnan(X), axis=1)
+                    X[invalid, :] = 0.0
+                    y = rfr.predict(X)
+                    y[invalid] = np.nan
+                    dst.write(y[np.newaxis, np.newaxis, ...], window=window)
+                
     # use meshgrid to create predictions
     # pass
-    # plot predicted versus true; variable importance 
-    #slope vs. ndwi for average NDVI, etc. plot for two slopes: NDVI, NDWI for average ruggedness, elevation, 
+    # plot predicted versus true; variable importance
+    # slope vs. ndwi for average NDVI, etc. plot for two slopes: NDVI, NDWI for average ruggedness, elevation,
     # all east-facing
 
-
-
 if __name__ == '__main__':
-    df = pd.read_csv(os.path.join(path0, "sample.csv"), index_col=0)
-    df.dropna(subset=['ebar'], inplace=True)
     fnrf = os.path.join(path0, 'rfr.joblib')
     fnimp = os.path.join(path0, 'importance.joblib')
-    
+
     # run_RF(df, fnout=fnrf)
     # variable_importance(df, fnrf, fnimp)
 
-    rfr = joblib.load(fnrf)
-    impres = joblib.load(fnimp)
-    # plot_fit(df, rfr, impres)
-    plot_pred(df, rfr)
+
+    # fnim = os.path.join(path0, 'covariates.tif')
+    # fnout = os.path.join(path0, 'e_pred.tif')
+    # predict_image(fnim, rfr, fnout)
---
+++ scripts/kivalina_RF_plots.py
@@ -0,0 +1,193 @@
+'''
+Created on Aug 29, 2023
+
+@author: simon
+'''
+import numpy as np
+import joblib
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from scripts.kivalina_RF import xy_split, predict_grid, path0, df
+from scripts.plotting import prepare_figure, colslist, cmap_e
+
+def plot_fit(df, rfr, impres):
+    import matplotlib.pyplot as plt
+    fig, axs = prepare_figure(ncols=2, figsize=(1.0, 0.4), sharex=False, sharey=False, wspace=1)
+    X, y = xy_split(df, train_test=False)
+    axs[0].plot(y, rfr.predict(X), linestyle='none', mec='none', mfc='k', ms=1, marker='o', alpha=0.1)
+    fitype = 'permutation'
+    ind = np.argsort(impres[fitype])
+    print(impres[fitype])
+    axs[1].plot(impres[fitype][ind], np.arange(len(ind)), linestyle='none', marker='o')
+    axs[1].set_yticks(np.arange(len(ind)))
+    axs[1].set_yticklabels(np.array(df.columns)[1:][ind])
+    axs[1].set_xlim((0.0, 0.7))
+    plt.show()
+
+def plot_pred(df, rfr):
+    vlim = (0.0, 0.5)
+    variables = ('slope', 'NDWI')
+    fixed_values = {'northerliness': 0.0, 'easterliness': 1.0, 'DEM_bp': 0, 'rugged': 30}
+    fixed_values_plot = [{'ndvi': 0.25}, {'ndvi': 0.75}]
+    # dem_bp also has big influence, but RF did not learn rocky (DEM_bp >>0,NDVI~0, NDWI<0) well
+    import matplotlib.pyplot as plt
+    from scripts.plotting import prepare_figure, cmap_e
+    fig, axs = prepare_figure(
+        ncols=len(fixed_values_plot), figsize=(1.6, 0.5), sharex=False, sharey=False, wspace=1, bottom=0.22)
+    for jp, _fv in enumerate(fixed_values_plot):
+        fv = {**fixed_values, **_fv}
+        gridvar, y_p = predict_grid(df, rfr, variables, fixed_values=fv)
+        print(gridvar[1])
+        axs[jp].imshow(
+            y_p, extent=(gridvar[0][0], gridvar[0][-1], gridvar[1][0], gridvar[1][-1]), aspect='auto',
+            vmin=vlim[0], vmax=vlim[1], cmap=cmap_e, origin='lower')
+        axs[jp].set_xlabel(variables[0])
+        axs[jp].set_ylabel(variables[1])
+    plt.show()
+
+def conditional_expectation(
+        rfr, X, covariate, ranges_dict=None, covariate_range=None, covariate_steps=64, subsample=10, 
+        rng=999):
+    # single covariate
+    try:
+        rng.random()
+    except:
+        rng = np.random.default_rng(rng)
+    if covariate_range is None:
+        covariate_range = np.nanpercentile(X[covariate], (1, 99))
+    covariate_grid = np.linspace(*covariate_range, covariate_steps)
+    _X = X
+    if ranges_dict is not None:
+        for var in ranges_dict:
+            if ranges_dict[var][0] is not None:
+                _X = _X[_X[var] > ranges_dict[var][0]]
+            if ranges_dict[var][1] is not None:
+                _X = _X[_X[var] <= ranges_dict[var][1]]
+    if subsample is not None and len(_X) > subsample:
+        _X = _X.iloc[rng.permutation(len(_X))[0:subsample]]
+    y = []
+    for ind in range(len(_X)):
+        df = pd.DataFrame(_X.iloc[ind:ind + 1])
+        df = df.loc[np.repeat(df.index.values, covariate_steps)]
+        df[covariate] = covariate_grid
+        y_ind = rfr.predict(df)
+        y.append(y_ind)
+    dict_res = {'X': _X, 'y_pred_cov': np.array(y).T, 'grid': covariate_grid,
+                'covariate': covariate, 'ranges_dict': ranges_dict}
+    return dict_res
+
+def plot_conditional_expectation(ce_res, ax, cmap=None, clim=(0, 1), ccov=None, alpha=0.5):
+    if cmap is None:
+        import matplotlib
+        cmap = matplotlib.colormaps['viridis']
+    grid = ce_res['grid']
+    print(ce_res['X'][ccov])
+    if ccov is not None:
+        cvals = (np.array(ce_res['X'][ccov]) - clim[0]) / (clim[1] - clim[0])
+        c = cmap(cvals)
+    else:
+        c = cmap(np.ones_like(ce_res['y_pred_cov']))
+    for _y, _c in zip(ce_res['y_pred_cov'].T, c):
+        ax.plot(grid, _y, alpha=alpha, c=_c)
+    print(cvals)
+
+def plot_rf(df, rfr, impres):
+    from sklearn.metrics import mean_squared_error
+    from matplotlib.transforms import Bbox
+    from matplotlib.colors import Normalize
+    from matplotlib import cm
+    from string import ascii_lowercase
+    fig, axs = prepare_figure(
+        ncols=5, figsize=(2.03, 0.39), sharex=False, sharey=False, wspace=0.670, bottom=0.178, top=0.968,
+        right=0.990, left=0.100)
+    dx = -0.03
+    cbarl = 0.94
+    _, X, _, y = xy_split(df, train_test=True)
+    axs[1].plot((0, 1), (0, 1), lw=0.5, alpha=0.3, c=colslist[1])
+    pred = rfr.predict(X)
+    axs[1].plot(
+        y, pred, linestyle='none', mec='none', mfc=colslist[0], ms=1, marker='o', alpha=0.1)
+    RMSE = mean_squared_error(y, pred, squared=False)
+    axs[1].text(0.04, 0.90, f'RMSE {RMSE:1.2f}', transform=axs[1].transAxes, ha='left')
+    lims = (0.00, 0.65)
+    ticks = (0.0, 0.3, 0.6)
+    pos_xl = (0.50, -0.41)
+    pos_yl = (-0.37, 0.45)
+    axs[1].set_xlim(lims)
+    axs[1].set_ylim(lims)
+    axs[1].set_yticks(ticks)
+    axs[1].text(
+        *pos_xl, '$\\hat{\\bar{e}}$ [$-$]', ha='center', va='baseline', transform=axs[1].transAxes)
+    axs[1].text(
+        *pos_yl, 'RF $\\bar{e}$ [$-$]', ha='right', va='center', transform=axs[1].transAxes, rotation=90)
+    fitype = 'permutation'
+    ind = np.argsort(impres[fitype])
+    # print(impres[fitype])
+    axs[0].plot(
+        impres[fitype][ind], np.arange(len(ind)), linestyle='none', marker='o', ms=2, mec='none',
+        mfc=colslist[0])
+    covariate_labels = {
+        'ndvi': 'NDVI', 'NDWI': 'NDWI', 'elevation': 'elevation', 'rugged': 'ruggedness',
+        'DEM_bp': 'elev. bp', 'slope': 'slope', 'northerliness': 'north', 'easterliness': 'east'}
+    axs[0].set_yticks(np.arange(len(ind)))
+    axs[0].set_yticklabels([covariate_labels[x] for x in np.array(df.columns)[1:][ind]])
+    axs[0].set_xlim((0.0, 0.7))
+    axs[0].text(
+        *pos_xl, 'importance [-]', ha='center', va='baseline', transform=axs[0].transAxes)
+
+    # from sklearn.inspection import PartialDependenceDisplay
+    # dpd = PartialDependenceDisplay.from_estimator(
+    #     rfr, X, ['slope'], kind='individual', ax=axs[2], subsample=100)
+    # print(dpd)
+    # PartialDependenceDisplay.from_estimator(
+    #     rfr, X, ['slope', 'NDWI', 'ndvi'], ax=axs[2], subsample=100)
+    # print(np.count_nonzero(X['ndvi'] > 0.73))
+    ccov, clim = 'NDWI', (-0.05, 0.65)
+    cmap, alpha = cmap_e, 0.5
+    ndvi_ranges = [(None, 0.30), (0.30, 0.75), (0.75, None)]
+    subsample = 20
+    for jndvir, ndvir in enumerate(ndvi_ranges):
+        ce_res = conditional_expectation(
+            rfr, X, 'slope', covariate_range=(0.5, 20), ranges_dict={'ndvi': ndvir}, subsample=subsample)
+        ax =  axs[2 + jndvir]
+        plot_conditional_expectation(
+            ce_res, ax, cmap=cmap, clim=clim, ccov=ccov, alpha=alpha)
+        if jndvir > 0:
+            ax.set_yticklabels([])
+        ax.set_ylim(lims)
+    axs[2].text(
+        *pos_yl, 'RF $\\bar{e}$ [$-$]', ha='right', va='center', transform=axs[2].transAxes, rotation=90)
+    
+    n_shift = 0
+    labels = ['importance', 'fit', 'low NDVI', 'medium NDVI', 'high NDVI']
+    for jax, ax in enumerate(axs):
+        ax.set_aspect(1 / ax.get_data_ratio())
+        label = f'{ascii_lowercase[jax]}) {labels[jax]}'
+        ax.text(0.00, 1.06, label, ha='left', va='baseline', transform=ax.transAxes)
+        if jax in [2, 3, 4]:
+            apos = ax.get_position()
+            ax.set_yticks(ticks)
+            ax.set_position(Bbox.from_bounds(apos.x0 + n_shift * dx, apos.y0, apos.width, apos.height))
+            ax.text(
+                *pos_xl, 'slope [$^{\\circ}]$', ha='center', va='baseline', transform=ax.transAxes)
+            n_shift = n_shift + 1
+    dh = 0.08
+    cax = fig.add_axes((cbarl, apos.y0 + dh, 0.01, apos.height-2*dh))
+    cbar = fig.colorbar(cm.ScalarMappable(norm=Normalize(*clim, clip=True), cmap=cmap), cax=cax)
+    cbarlabel = 'NDWI [-]'
+    cax.text(2.40, 1.07, cbarlabel, ha='center', va='baseline', transform=cax.transAxes)            
+    plt.show()
+
+if __name__ == '__main__':
+    fnrf = os.path.join(path0, 'rfr.joblib')
+    fnimp = os.path.join(path0, 'importance.joblib')
+    rfr = joblib.load(fnrf)
+    impres = joblib.load(fnimp)
+
+    plot_rf(df, rfr, impres)
+
+    # plot_fit(df, rfr, impres)
+
+    # plot_pred(df, rfr)
