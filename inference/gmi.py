c6710f61e036f757b27051a22b29e4d1863f769e
Simon Zwieback <szwieback@alaska.edu>
2024-09-26 11:39:07 -0800

    GMI comments

--- inference/gmi.py
+++ inference/gmi.py
@@ -5,7 +5,7 @@ Created on Feb 29, 2020
 '''
 import numpy as np
 from sklearn.mixture import BayesianGaussianMixture, GaussianMixture
-from scipy.linalg import solve_triangular
+from scipy.linalg import solve_triangular, cholesky
 from scipy.stats import multivariate_normal
 
 from inference.isi import _sqr_eigen, _nondata_terms_mvnormal, invert_nonzero, sumlogs
@@ -20,7 +20,7 @@ def fit_gaussian_mixture(
 
     return gm
 
-def _sqr(A, cond_thresh=1e-6,):
+def _sqr(A, cond_thresh=1e-6):
     lam_sqr, Q, ind = _sqr_eigen(A, cond_thresh=cond_thresh)
     sqr = Q * lam_sqr[:, np.newaxis,:]
     lam_sqr[ind['singular']] = 1
@@ -42,12 +42,10 @@ def _condition_gm(
     P = y_obs.shape[1]
     M = y_obs.shape[0]
     Q = mu.shape[1]  # dimension of prior RV considered
-
-    if V is None and ind_V is None:
-        V, V_invT, ind_V, V_lam_sqr = _sqr(C_obs, cond_thresh=cond_thresh)
-
+    Qout = Q if not 'unobserved' in method_condition else Q - P
+    
     q_y = Q - P
-    if method_condition == 'square_root':
+    if method_condition == 'sqr':
         # square root implementation following Angus Andrews:
         # A Square Root Formulation of the Kalman Covariance Equations
         Wp_k = solve_triangular(gm.precisions_cholesky_[k, ...], np.eye(Q)).T
@@ -55,6 +53,8 @@ def _condition_gm(
         U_k, U_invT_k, ind_U_k, U_k_lam_sqr = _sqr(
             Sigma[k, q_y:, q_y:] + C_obs, cond_thresh=cond_thresh)
         y_k_prior = mu[k, q_y:]
+        if V is None and ind_V is None:
+            V, _, ind_V, _ = _sqr(C_obs, cond_thresh=cond_thresh)        
         UpV_k_inv = np.linalg.pinv(U_k + V, rcond=cond_thresh)
         UPD_k = np.eye(Q)[np.newaxis, ...] - np.einsum(
             'qb, mbc, mcd, ed -> mqe', Z_k, U_invT_k, UpV_k_inv, Z_k, optimize=True)
@@ -73,7 +73,7 @@ def _condition_gm(
     elif method_condition in ('full', 'unobserved'):
         # for checking; does not handle eigenvalues near or < 0 well
         y_k_prior = mu[k, q_y:]
-        # only compute for idnices up to q_y if unobserved
+        # only compute for indices up to q_y if unobserved
         sl = slice(None, None) if method_condition == 'full' else slice(None, q_y)
         Sigma_k_off = Sigma[k,sl, q_y:]
         # adding observation covariance (works if observation noise is Gaussian)
@@ -84,8 +84,9 @@ def _condition_gm(
         mu_k = mu[np.newaxis, k, sl]
         Sigma_k = Sigma[np.newaxis, k, sl, sl]
         # update mu and Sigma using standard mv normal 
-        mu_p_k = mu_k + np.einsum('pb, mbc, mc -> mp', Sigma_k_off, Sigma_k_obs_inv, dy)
-        Sigma_p_k = Sigma_k - np.einsum('pb, mbc, dc -> mpd', Sigma_k_off, Sigma_k_obs_inv, Sigma_k_off)
+        mu_p_k = mu_k + np.einsum('pb, mbc, mc -> mp', Sigma_k_off, Sigma_k_obs_inv, dy, optimize=True)
+        Sigma_p_k = Sigma_k - np.einsum(
+            'pb, mbc, dc -> mpd', Sigma_k_off, Sigma_k_obs_inv, Sigma_k_off, optimize=True)
         # compute new weight for Gaussian mixture (not normalzied yet)
         log_p_y_obs = -0.5 * (
             P * np.log(2 * np.pi) + np.linalg.slogdet(Sigma_k_obs)[1]
@@ -110,7 +111,7 @@ def posterior_gm_mvnormal(
     Qout = Q if not 'unobserved' in method_condition else Q - P
     assert C_obs.shape == (M, P, P)
 
-    V, V_invT, ind_V, V_lam_sqr = _sqr(C_obs, cond_thresh=cond_thresh)
+    V, _, ind_V, _ = _sqr(C_obs, cond_thresh=cond_thresh)
 
     logpi_p = np.empty((K, M))
     mu_p = np.empty((K, M, Qout))
@@ -121,6 +122,7 @@ def posterior_gm_mvnormal(
             y_obs, C_obs, gm, k=k, V=V, ind_V=ind_V, cond_thresh=cond_thresh,
             method_condition=method_condition)
 
+    # clean up
     ind_invalid = np.any(np.isnan(logpi_p), axis=0)
     logpi_p[:, ind_invalid] = 0.0
     # normalize weights
@@ -133,6 +135,7 @@ def posterior_gm_mvnormal(
 def posterior_moments(mu_p, Sigma_p, pi_p):
     mu = np.einsum('ijk, ij -> jk', mu_p, pi_p)
     mu_dev = mu_p - mu[np.newaxis, ...]
+    # from Mode-finding for mixtures of Gaussian distributions
     Sigma = np.einsum(
         'ijkl, ij -> jkl', Sigma_p + mu_dev[..., np.newaxis] * mu_dev[..., np.newaxis,:], pi_p)
     return mu, Sigma
@@ -144,7 +147,7 @@ if __name__ == '__main__':
     rs = np.random.RandomState(seed=5)
     parms = rs.uniform(low=0.0, high=0.8, size=(N, 1))
     y_ref = parms * H
-    y_ref += rs.normal(scale=0.  , size=(N, P))
+    y_ref += rs.normal(scale=0.3 , size=(N, P))
 
     # fit
     samples = np.concatenate((parms, y_ref), axis=-1)
@@ -157,8 +160,9 @@ if __name__ == '__main__':
     y_obs[...] += parm0[np.newaxis,:] * H
     C_obs = np.zeros((M, P, P))
     C_obs[:, ...] = sigma_obs ** 2 * np.eye(P)[np.newaxis, ...]
-    mu_p, Sigma_p, pi_p = posterior_gm_mvnormal(y_obs, C_obs, gm, method_condition='full')
-    mu_pu, Sigma_pu, pi_pu = posterior_gm_mvnormal(y_obs, C_obs, gm, method_condition='unobserved')  # need to cut this down
+    mu_p, Sigma_p, pi_p = posterior_gm_mvnormal(y_obs, C_obs, gm, method_condition='sqr')
+    mu_pu, Sigma_pu, pi_pu = posterior_gm_mvnormal(y_obs, C_obs, gm, method_condition='unobserved')
+
 
     # print(Sigma_pu[:, :, 0, 0] - Sigma_p[:, :, 0, 0])
     print(pi_p - pi_pu)
@@ -166,6 +170,4 @@ if __name__ == '__main__':
     # mu, Sigma = posterior_moments(mu_p, Sigma_p, pi_p)
     # print(mu_p.shape, mu.shape)
 
-    # add square root smaller subset
-    # document properly 
-
+    # need to refactor inversion and synthetic to be more general
