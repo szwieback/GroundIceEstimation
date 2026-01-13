ac17c1fcb54c2bb92736bd85f58588c87f81d449
Simon Zwieback <szwieback@alaska.edu>
2026-01-12 15:19:05 -0900

    hdf5 defo export bug

--- analysis/ioput.py
+++ analysis/ioput.py
@@ -524,7 +524,7 @@ def hdf5_attributes(nodata=np.nan, dtype='float32', geospatial=None):
 def export_defo_history_hdf5(
         s_obs, pout, geospatial, geom, dates_obs_str=None, K=None, flip_sign=True,
         fn_defo='defo_history.h5', fn_K_diag='defo_history_covariance_diagonal.h5'):
-    attributes = geospatial._hdf5_attributes()
+    attributes = hdf5_attributes(geospatial)
     attributes['INC_ANGLE'] = geom['ia'] * 180 / np.pi  # degrees
     if flip_sign:
         s_obs = s_obs * (-1)  # so subsidence is negative
